fe22c013b1962fc975c3d357d000278c097417ff
grushilkumar <43177668+grushilkumar@users.noreply.github.com>
2024-02-25 16:57:33 +0530

    Time: 460 ms (86.23%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,3 @@
 -- Write your PostgreSQL query statement below
 select e.name from Employee e where e.id in 
-(select managerId from Employee group by managerId having count(1)>=5)
+(select managerId from Employee group by managerId having count(1)>=5);
