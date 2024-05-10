06d0dd68ad041b6eec42dba7f65817469552ff8d
G Rushil Kumar <43177668+grushilkumar@users.noreply.github.com>
2024-05-10 18:51:01 +0530

    Time: 627 ms (40.32%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,12 +1,13 @@
 -- Write your PostgreSQL query statement below
-select user_id,
-case when confirmation_rate != 0 then confirmation_rate
-else 0
-end as confirmation_rate 
-from
-(select
-s.user_id,
-round(sum(case when c.action= 'confirmed' then 1 end)/count(c.user_id):: numeric,2) as confirmation_rate 
-from Signups s 
-left join confirmations c on s.user_id = c.user_id
-group by s.user_id);
+
+with conf as(select user_id,count(1) from confirmations where action = 'confirmed' group by user_id),
+total as(select user_id,count(1) from confirmations group by user_id),
+conf_tab as(select s.user_id,c.count as conf_count from signups s left join conf  c on  s.user_id = c.user_id),
+total_tab as(select s.user_id,c.count as total_count from signups s left join total  c on  s.user_id = c.user_id)
+
+
+select c.user_id,
+case
+    when conf_count is null then 0 else round(conf_count/total_count::numeric,2) end as confirmation_rate    
+from conf_tab c
+join total_tab t on c.user_id = t.user_id;
